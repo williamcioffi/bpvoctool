5a2b460756a63f2d9f55a092bb7b63fe35b94a11
orangeminke <orangeminke@gmail.com>
2016-03-16 16:30:19 -0400

    Added files via upload

---
+++ openall.m
@@ -0,0 +1,42 @@
+function [fnames dirpath nfiles] = openall()
+% OPENALL returns names of wav files in the selected directory
+%   usage: [fnames, dirpath, nfiles] = openall();
+%       fnames      -- a celery of filenames
+%       dirpath     -- a string representing the path to the directory
+%                      selected
+%       nfiles      -- a 1x1 array containing number of wav files
+%
+%   last modified: 28Jan2016
+%   to do: formal exception handling
+%~wrc
+
+fnames = [];
+nfiles = [];
+
+dirpath = uigetdir(pwd, 'select input dir');
+if(dirpath == 0)
+    error('did not select a directory...');
+end
+
+files = dir(dirpath);
+
+files_cell = struct2cell(files);
+fnames = files_cell(1, :);
+
+%get rid of directories if there are any
+isdir = files_cell(4, :);
+keeps = find(cellfun(@(x) x == 0, isdir) == 1);
+fnames = fnames(keeps);
+
+% grab only file names which end in .wav
+wavs = regexp(fnames, '[.]wav$');
+keeps = ~cellfun('isempty', wavs);
+
+if(keeps == 0)
+    error('no wav files in directory...');
+end
+
+fnames = fnames(keeps);
+nfiles = length(fnames);
+
+end
