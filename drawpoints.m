d760c15120ec1125d52b8819aec79e771732a9ba
orangeminke <orangeminke@gmail.com>
2016-08-30 10:27:49 -0400

    Update drawpoints.m

--- drawpoints.m
+++ drawpoints.m
@@ -1,5 +1,6 @@
 function drawpoints(a, b, curclick)
 
-text(a, b, num2str(curclick), 'color', 'red');
+%text(a, b, num2str(curclick), 'color', 'red');
+text(a, b, char(curclick), 'color', 'red');
 
 end
